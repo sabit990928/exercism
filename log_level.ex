88f49aacfce255a9576232f2de46dd995fb1bb5e
Sabit <sabit990928@gmail.com>
2022-11-30 00:46:25 +0600

    Add log level with initial case implementation

---
+++ log_level.ex
@@ -0,0 +1,24 @@
+defmodule LogLevel do
+  def to_label(level, legacy?) do
+    case {level, legacy?} do
+      {0, false} -> :trace
+      {1, _} -> :debug
+      {2, _} -> :info
+      {3, _} -> :warning
+      {4, _} -> :error
+      {5, false} -> :fatal
+      {_, _} -> :unknown
+    end
+  end
+
+  def alert_recipient(level, legacy?) do
+    label = to_label(level, legacy?)
+
+    case {label, legacy?} do
+      {:unknown, true} -> :dev1
+      {:unknown, false} -> :dev2
+      {:error, _} -> :ops
+      {:fatal, _} -> :ops
+      _ -> false
+  end
+end
